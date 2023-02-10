064ba4ab2fb5e708d001afa3cc87400a12918246
art3kr <art3kr@virginia.edu>
2023-02-10 09:14:46 -0500

    Added Assignment01

---
+++ Assignment01/js/script.js
@@ -0,0 +1,112 @@
+// STEP 1
+// let someMonth;
+// function theMonth() {
+    
+// };
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2 - literals
+// 120; // numeric
+// 'Hello World!'; // string
+// true; // boolean
+// null; // null
+
+// STEP 3
+// let var1 = 34 - 21;
+// let arr1 = ['hello', 'goodbye', 'ciao'];
+
+// STEP 4
+// let strFirstName, strLastName, intYourAge;
+// let strAddress, strCity, strState, intZipCode;
+// let strReferralSource, boolMayWeContactYou;
+
+// STEP 5
+// strFirstName = "Antonio";
+// boolMayWeContactYou = false;
+// intYourAge = 28;
+
+// STEP 6
+// let output = 'my age is: ' + 28;
+// console.log(output);
+
+// STEP 7
+// let output2 = 'are you allowed to contact me?: ' + false;
+// console.log(output2);
+// let output3 = true + 28;
+// console.log(output3);
+
+// STEP 8
+// answer - no it is not valid.  Yout need an escape character for the apostrophe in I'm.  See below
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+// const nullVal = null, undefVal = undefined;
+// console.log(nullVal), console.log(undefVal);
+
+// STEP 10
+// console.log(typeof('Hello')), console.log(typeof(28)), console.log(typeof(true)); 
+// console.log(typeof({key: 'value'})), console.log(typeof(undefined));
+
+// STEP 11
+// console.log('Hello ' + 'Antonio Trani' + ', welcome to the JavaScript class!');
+
+// STEP 12
+// let strName = 'Antonio Trani';
+// console.log('Hello ' + strName + ', welcome to the JavaScript class!');
+
+// STEP 13
+// course = 'JavaScript';
+// console.log('Hello ' + strName + ', welcome to the ' + course + ' class!');
+
+// STEP 14
+// console.log('Hello ' + strName + '.\nWelcome to the ' + course + 'class!');
+
+// STEP 15
+// strName = prompt('Enter your name.')
+// console.log('Hello ' + strName + '.\nWelcome to the JavaScript class!');
+
+// STEP 16
+// course = prompt('What course are you taking?')
+// console.log('Hello Antonio.\nWelcome to the ' + course + ' class!');
+
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// let toCompare = 50;
+// if (toCompare < 100 && toCompare > 20) {
+//     result = true;
+// } else {
+//     result = false;
+// }
+// console.log(result);
+
+// STEP 22
+// let toCompare = 50;
+// if (toCompare == 10 || toCompare == 20) {
+//     result = true;
+// } else {
+//     result = false;
+// };
+// console.log(result);
