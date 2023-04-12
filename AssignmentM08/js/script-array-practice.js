0ee402c504b59b8f5b77a40fc7f7829a7fc99046
art3kr <art3kr@virginia.edu>
2023-04-11 21:22:40 -0400

    added homework 8 stuff

---
+++ AssignmentM08/js/script-array-practice.js
@@ -0,0 +1,99 @@
+//STEP 1
+// let moviesArray = ['Mad Max','Blade Runner','Die Hard', 'Dune','John Wick'];
+// console.log(moviesArray[1]);
+
+//STEP 2 
+// let moviesArray = ['Mad Max','Blade Runner','Die Hard', 'Dune','John Wick'];
+// let movies = new Array(5);
+// for (let i = 0; i<movies.length; i++) {
+//     movies[i] = moviesArray[i];
+// }
+// console.log(movies[0]);
+
+//STEP 3 
+// let moviesArray = ['Mad Max','Blade Runner','Die Hard', 'Dune','John Wick'];
+// let movies = new Array(5);
+// for (let i = 0; i<movies.length; i++) {
+//     movies[i] = moviesArray[i];
+// }
+// movies.splice(2, 0, "Catch me if You Can");
+// console.log(movies.length);
+
+//STEP 4
+// let moviesArray = [];
+// moviesArray.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick');
+// delete moviesArray[0];
+// console.log(moviesArray);
+
+//STEP 5
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// for (let element in movies) {
+//     console.log(movies[element]);
+// }
+
+//STEP 6
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// for (let element of movies) {
+//     console.log(element);
+// }
+
+//STEP 7
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// movies = movies.sort();
+// for (let element in movies) {
+//     console.log(movies[element]);
+// }
+
+//STEP 8
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// moviesIRegret = ['Dont Worry Darling','50 First Dates', 'Black Beauty'];
+
+// console.log('Movies I like: \n\n')
+// for (let element in movies) {
+//     console.log(movies[element]);
+// }
+
+// console.log('\nMovies I regret watching: \n\n')
+// for (let element in moviesIRegret) {
+//     console.log(moviesIRegret[element]);
+// }
+
+//STEP 9
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// moviesIRegret = ['Dont Worry Darling','50 First Dates', 'Black Beauty'];
+// movies = movies.concat(moviesIRegret);
+// movies = movies.sort();
+// movies = movies.reverse();
+
+// for (let element of movies) {
+//     console.log(element);
+// }
+
+//STEP 10
+// let movies = [];
+// movies.push('Mad Max','Blade Runner','Die Hard', 'Dune','John Wick','Fast and Furious','Star Wars');
+// moviesIRegret = ['Dont Worry Darling','50 First Dates', 'Black Beauty'];
+// movies = movies.concat(moviesIRegret);
+// movies = movies.sort();
+// movies = movies.reverse();
+// lastElement = movies.pop();
+// console.log(lastElement);
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
