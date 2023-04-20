ae54d52d488fa7484a67b5b002fa5c13e9f71da3
Antonio Trani <39489520+art3kr@users.noreply.github.com>
2023-04-20 01:01:45 -0500

    Add files via upload

---
+++ AssignmentM09/js/script.js
@@ -0,0 +1,56 @@
+// Import init.js module
+import { jsonLoader } from './modules/init.js';
+
+// Get json data object from json file
+let employeesJSON = await jsonLoader().json();
+for (let employee of employeesJSON) {
+    console.log(employee);
+}
+
+
+// GET DOM ELEMENTS
+let empTable    = document.querySelector('#employees')
+let empCount    = document.querySelector('#empCount')
+
+// BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
+buildGrid(arrEmployees)
+
+// DELETE EMPLOYEE
+empTable.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+        // CONFIRM THE DELETE
+        if (confirm('Are you sure you want to delete this employee?')) {
+            // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+            let rowIndex = e.target.parentNode.parentNode.rowIndex
+            // REMOVE EMPLOYEE FROM ARRAY
+            empTable.deleteRow(rowIndex)
+        }
+    }
+})
+
+// BUILD THE EMPLOYEES GRID
+function buildGrid(arrEmployees) {
+    // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
+    empTable.lastElementChild.remove()
+    // REBUILD THE TBODY FROM SCRATCH
+    let tbody = document.createElement('tbody')
+    // LOOP THROUGH THE ARRAY OF EMPLOYEES
+    // REBUILDING THE ROW STRUCTURE
+    for (let employee of arrEmployees) {
+        tbody.innerHTML += 
+        `
+        <tr>
+            <td>${employee[0]}</td>
+            <td>${employee[1]}</td>
+            <td>${employee[2]}</td>
+            <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+            <td>${employee[4]}</td>
+            <td><button class="btn btn-sm btn-danger delete">X</button></td>
+        </tr>
+        `
+    }
+    // BIND THE TBODY TO THE EMPLOYEE TABLE
+    empTable.appendChild(tbody)
+    // UPDATE EMPLOYEE COUNT
+    empCount.value = `(${arrEmployees.length})`
+}
