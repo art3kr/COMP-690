ea162b0992011c8e4fe6506ca7f0d6109b1d222f
Antonio <art3kr@virginia.edu>
2023-05-10 15:48:41 -0400

    added M11 files

--- AssignmentM11/js/strings.js
+++ AssignmentM11/js/strings.js
@@ -1,18 +1,38 @@
 //STEP 1
+// let name = prompt("What is your name?");
+// alert(name.length);
 
 //STEP 2
+// let name = prompt("What is your name?");
+// let position = prompt("Give me a number");
+// alert(name[position]);
 
 //STEP 3
+// let firstName = prompt("What is your first name?");
+// let lastName = prompt("What is your last name?");
+// alert(firstName.concat(` ${lastName})`));
 
 //STEP 4
+// let defaultText = "The quick brown fox jumps over the lazy dog";
+// alert(defaultText.search("fox"));
 
 //STEP 5
+// let defaultText = "The quick brown fox jumps over the lazy fox";
+// alert(defaultText.lastIndexOf("fox"));
 
 //STEP 6
+// let defaultText = "The quick brown fox jumps over the lazy dog";
+// let firstName = prompt("What is your first name?");
+// alert(defaultText.replace("the lazy dog", firstName));
 
 //STEP 7
+// let old_string = "The quick brown fox jumps over the lazy dog";
+// let new_string = old_string.slice(30,45);
+// alert(new_string.toUpperCase());
 
 //STEP 8
+let defaultText = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+alert(defaultText.trim().toLowerCase());
 
 //STEP 9
 
